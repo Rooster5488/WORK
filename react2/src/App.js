dbaf12146168bb6993cef108de7b2536919af9e7
nwPhoenix <n11076933@qut.edu.au>
2022-11-06 15:04:31 +1000

    UI bug fixes
    still working on links

--- react2/src/App.js
+++ react2/src/App.js
@@ -32,19 +32,26 @@ function App() {
         </nav>
       </nav>
         <div>
-        <Room roomId={1}/>
-        <Room roomId={2}/>
-        <Room roomId={3}/>
-        <Room roomId={4}/>
-        <Room roomId={5}/>
-        <Room roomId={6}/>
-        <Room roomId={7}/>
+          <Room roomId={1}/>
+          <Room roomId={2}/>
+          <Room roomId={3}/>
+          <Room roomId={4}/>
+          <Room roomId={5}/>
+          <Room roomId={6}/>
+          <Room roomId={7}/>
         </div>
         <div>
           <button class = "exportIcon" onClick ={() => setExportPopup(true)}>EXPORT</button>
           <Popup trigger = {exportIcon1} setTrigger={setExportPopup}>
-                  <h3>Export Overlay</h3>
-                  <p>Lorem Ipsum</p>
+            <h3>Export Overlay</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex 
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
           </Popup>
         </div>        
       </div>
